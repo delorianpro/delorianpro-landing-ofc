7cdb353313ea4c925fa129a1a7c03dffa6e01237
Delorianpro <delorianpro@gmail.com>
2025-03-09 16:25:19 -0300

    Adiciona tag do google e arquivo robots txt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { title_font, subtitle_font } from "@/functions/fonts";
+import Script from "next/script";
 
 
 export const metadata: Metadata = {
@@ -15,6 +16,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
+      <head>
+        <Script
+          async
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer', 'AW-16762428286');
+            `,}}
+        />
+      </head>
       <body className={`${title_font.variable} ${subtitle_font.variable}`}>
         {children}
       </body>
