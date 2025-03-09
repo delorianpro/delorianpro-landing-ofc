26277082e5316d794836a2655696357e6c7493e1
Delorianpro <delorianpro@gmail.com>
2025-03-09 16:33:08 -0300

    Corrige tag do google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
               new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
               j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'AW-16762428286');
+              })(window,document,'script','dataLayer', 'G-KHJB6BBN0S');
             `,}}
         />
       </head>
