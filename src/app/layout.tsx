ff170ed8cd15f3e8b6f13e32ee372cd181244eda
Delorianpro <delorianpro@gmail.com>
2025-04-02 19:40:18 -0300

    Corrige o código da GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Delorian | Automação e Manutenção de portões eletrônicos",
-  description: "Automação e manutenção de portões eletrônicos. Instalação, conserto e eficiência. A Delorian oferece automação e manutenção de portões eletrônicos, instalação de cremalheiras, conserto de motores. Serviços especializados para segurança e conveniência, vamos até você sem cobrar nada e orçamento também gratuito. Confie na expertise da Delorian para soluções confiáveis e eficientes.",
+  description: "Automação e manutenção de portões eletrônicos. A Delorian oferece instalção, automação e manutenção de portões eletrônicos, instalação de cremalheiras, conserto de motores. Serviços especializados para segurança e conveniência, vamos até você sem cobrar nada e orçamento também gratuito.",
 };
 
 export default function RootLayout({
@@ -17,19 +17,20 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <Script
+         <Script
           async
-          id="google-tag-manager"
+          src="https://www.googletagmanager.com/gtag/js?id=G-KHJB6BBN0S"
           strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id=' + i + dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer', 'G-KHJB6BBN0S');
-            `,}}
         />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-KHJB6BBN0S');
+          `}
+        </Script>
       </head>
       <body className={`${title_font.variable} ${subtitle_font.variable}`}>
         {children}
